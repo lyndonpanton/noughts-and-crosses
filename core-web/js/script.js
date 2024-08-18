7638fab7a0ed491dafeb8461fca1dcc7accfd79f
Lyndon Panton <LyndonPanton@gmail.com>
2024-08-18 20:01:00 +0100

    Event listeners are add/removed on game start/end

--- core-web/js/script.js
+++ core-web/js/script.js
@@ -1,14 +1,13 @@
 const boardElements = document.getElementById("board");
 
 board = [];
-turn = "X";
+turn = "";
 
 for (let i = 0; i < boardElements.children.length; i++) {
     board[i] = boardElements.children[i];
-    board[i].addEventListener("click", cellClick, false);
 }
 
-/* 
+/*
     Win conditions
     
     1. 0 1 2
@@ -21,18 +20,51 @@ for (let i = 0; i < boardElements.children.length; i++) {
     8. 2 4 6
 */
 
+function startGame() {
+    // Users should be able to either:
+    // 1. Select who starts explicitly
+    // 2. State that each game should toggle between starting player
+    // 3. State that a random player should start
+    turn = "X";
 
+    // Add cell event listeners
+    for (let i = 0; i < boardElements.children.length; i++) {
+        board[i].addEventListener("click", cellClick, false);
+    }
+}
 
 function cellClick(e) {
     if (e.currentTarget.textContent === "") {
         e.currentTarget.textContent = turn;
 
         console.log(e.currentTarget.id.slice(5));
+        
+        // Check for win condition
+        let gameOver = checkWin(turn);
 
-        if (turn === "X") {
-            turn = "O"
-        } else {
-            turn = "X";
+        if (!gameOver) {
+            if (turn === "X") {
+                turn = "O"
+            } else {
+                turn = "X";
+            }
         }
+    } else {
+        // Denote that the cell is currently occupied
     }
+}
+
+function checkWin() {
+    
+    
+    return false;
+}
+
+function endGame() {
+    // Denote that a particular player has won
+    // Remove cell event listeners
+    for (let i = 0; i < boardElements.children.length; i++) {
+        board[i].removeEventListener("click", cellClick, false);
+    }
+    // Display next options: Play again, see stats, etc.
 }
